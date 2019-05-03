f3800a1cf43e85ac5609d1a15d2dd0659308c6e8
jalooc <jalooc@gmail.com>
2019-05-03 23:44:42 +0200

    Updates

---
+++ precache-manifest.e9a55cd8013ead54e03dd3b3f5654575.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f9c011fa3c776407f668a65db88a13",
+    "url": "/c-pc-perftest/index.html"
+  },
+  {
+    "revision": "23f7b8775c88d55fe680",
+    "url": "/c-pc-perftest/static/js/2.6005b029.chunk.js"
+  },
+  {
+    "revision": "0cdd7cf352012cb609e0",
+    "url": "/c-pc-perftest/static/js/main.f778ebae.chunk.js"
+  },
+  {
+    "revision": "3a65042d990f89dd4864",
+    "url": "/c-pc-perftest/static/js/runtime~main.22cc9044.js"
+  }
+]);
